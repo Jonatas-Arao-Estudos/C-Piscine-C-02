8135c5c97d415d100b4a28843a0833036fc97f03
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-22 19:27:19 -0300

    ex01 added

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 19:04:09 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/22 19:14:30 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (src[index] != '\0' && index < n)
+	{
+		dest[index] = src[index];
+		index = index + 1;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
