335b79010eec50a4cfd33158185e5aca0dd12ab2
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-26 12:36:49 -0300

    ex03 added

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 11:33:18 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/26 12:20:11 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	int	index;
+
+	index = 0;
+	if (str[index] == -1 || str[index] == '\0')
+		return (1);
+	while (str[index] != '\0')
+	{
+		if (str[index] < '0' || str[index] > '9')
+			return (0);
+		index = index + 1;
+	}
+	return (1);
+}
