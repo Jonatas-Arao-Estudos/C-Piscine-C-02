49d770628fde10104b2f2cfe7c3bd3ddd5407c87
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-26 18:17:56 -0300

    ex08 added

---
+++ ex08/ft_strlowcase.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 16:32:00 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/26 16:32:25 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (str[index] >= 'A' || str[index] <= 'Z')
+			str[index] = str[index] + 32;
+		index = index + 1;
+	}
+	return (str);
+}
