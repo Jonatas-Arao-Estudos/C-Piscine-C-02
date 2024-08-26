61fae7d34fc22d7426d862b0a9cb8e4890ed8687
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-26 12:36:44 -0300

    ex02 added

---
+++ ex02/ft_str_is_alpha.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 11:15:18 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/26 12:20:07 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_alpha(char *str)
+{
+	int	index;
+
+	index = 0;
+	if (str[index] == -1 || str[index] == '\0')
+		return (1);
+	while (str[index] != '\0')
+	{
+		if ((str[index] < 'A' || str[index] > 'Z')
+			&& (str[index] < 'a' || str[index] > 'z'))
+			return (0);
+		index = index + 1;
+	}
+	return (1);
+}
