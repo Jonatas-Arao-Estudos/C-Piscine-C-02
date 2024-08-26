8bf70ab73994fa45b4b3403816e7766e25ec1285
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-26 12:38:15 -0300

    ex04 added

---
+++ ex04/ft_str_is_lowercase.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 11:48:10 by jarao-de          #+#    #+#             */
+/*   Updated: 2024/08/26 12:20:33 by jarao-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	index;
+
+	index = 0;
+	if (str[index] == -1 || str[index] == '\0')
+		return (1);
+	while (str[index] != '\0')
+	{
+		if (str[index] < 'a' || str[index] > 'z')
+			return (0);
+		index = index + 1;
+	}
+	return (1);
+}
