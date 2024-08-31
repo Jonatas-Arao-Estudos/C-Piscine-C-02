cec178fa13a7038cb231a70ed54eea44da3f2f26
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-31 16:53:53 -0300

    ex09 1st fix

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,32 +6,32 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 16:37:50 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/08/26 17:45:39 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/08/31 16:36:15 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_char_is_alpha(char str)
+int	ft_isalpha(char str)
 {
 	if ((str < 'A' || str > 'Z') && (str < 'a' || str > 'z'))
 		return (0);
 	return (1);
 }
 
-int	ft_char_is_lowercase(char str)
+int	ft_islowercase(char str)
 {
 	if (str < 'a' || str > 'z')
 		return (0);
 	return (1);
 }
 
-int	ft_char_is_uppercase(char str)
+int	ft_isuppercase(char str)
 {
 	if (str < 'A' || str > 'Z')
 		return (0);
 	return (1);
 }
 
-int	ft_char_is_numeric(char str)
+int	ft_isnumeric(char str)
 {
 	if (str < '0' || str > '9')
 		return (0);
@@ -41,16 +41,24 @@ int	ft_char_is_numeric(char str)
 char	*ft_strcapitalize(char *str)
 {
 	unsigned int	index;
+	unsigned int	capitalize;
 
 	index = 0;
+	capitalize = 1;
 	while (str[index] != '\0')
 	{
-		if (ft_char_is_lowercase(str[index]) == 1
-			&& (index == 0 || (ft_char_is_alpha(str[index - 1]) == 0
-					&& ft_char_is_numeric(str[index - 1]) == 0)))
-			str[index] = str[index] - 32;
-		else if (ft_char_is_uppercase(str[index]) == 1)
-			str[index] = str[index] + 32;
+		if (ft_isalpha(str[index]) == 1 || ft_isnumeric(str[index]) == 1)
+		{
+			if (ft_islowercase(str[index]) == 1 && capitalize == 1)
+				str[index] = str[index] - 32;
+			else if (ft_isuppercase(str[index]) == 1 && capitalize == 0)
+				str[index] = str[index] + 32;
+			capitalize = 0;
+		}
+		else
+		{
+			capitalize = 1;
+		}
 		index = index + 1;
 	}
 	return (str);
